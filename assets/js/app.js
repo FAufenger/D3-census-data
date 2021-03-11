a5496501ce0170f62d0df32def7782fc75ac535f
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-03-11 13:03:39 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 660;
+var svgWidth = 800;
+var svgHeight = 600;
 
 // Define the chart's margins as an object
 var chartMargin = { top: 30, right: 30, bottom: 30, left: 30 };
@@ -10,68 +10,77 @@ var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
 var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Select body, append SVG area to it, and set the dimensions
-var svg = d3.select("scatterPlot")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+var svg = d3.select("#scatterPlot")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
 
 // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 // Import data from data.csv 
-d3.csv("./assets/resources/data.csv").then(function(stateData) {
+d3.csv("./assets/resources/data.csv").then(function (stateData) {
     // CHeck to see what imported data looks like
     console.log(stateData);
+
+
+    // Cast the hours value to a number for each piece of tvData
+    stateData.forEach(function (d) {
+        d.age = +d.age;
+        d.smokes = +d.smokes;
+    });
+
+    // Create a linear scales for axis
+    var xLinearScale = d3.scaleLinear()
+        .domain([0, (d3.max(stateData, d => d.age) + 5)])
+        .range([0, chartWidth]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, (d3.max(stateData, d => d.smokes) + 5)])
+        .range([chartHeight, 0]);
+
+    // Create axis function
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale)  //.ticks(10);
+
+    // Append two SVG group elements to the chartGroup area,
+    // and create the bottom and left axes inside of them
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(xAxis);
+
+    chartGroup.append("g")
+        .call(yAxis);
     
-  /*
-  // Cast the hours value to a number for each piece of tvData
-  stateData.forEach(function(d) {
-    d.hours1 = +d.hours1;
-    d.hours2 = +d.hours2;
-  });
-  
-  // Create a linear scales for axis
-  var xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, d => d.hours)])
-    .range([chartHeight, 0]);
-  
-  var yLinearScale = d3.scaleLinear()
-  .domain([0, d3.max(stateData, d => d.hours)])
-  .range([chartHeight, 0]);
+    // Create one SVG circle per piece of stateData
+    // Use the linear scales to position each circle within the chart
+    chartGroup.selectAll(".circle")
+        .data(stateData)
+        .enter()
+        .append("circle") //.attr("class", "bar")
+        .attr("cx", d => xLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("r", "12")
+        .attr("stroke-width", "1")
+        .attr("fill", "green")
+        .attr("opacity", ".6");
+    
+        // Add axis labels
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
+        .attr("text-anchor", "middle")
+        .text(" Median Age");
 
-  // Create axis function
-  var bottomAxis = d3.axisBottom(xBandScale);
-  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);  
+    chartGroup.append("text")
+        .attr("y", 0 - ((margin.left / 2) + 2))
+        .attr("x", 0 - (height / 2))
+        .attr("text-anchor", "middle")
+        .text("smokers (%)");
 
-  // Append two SVG group elements to the chartGroup area,
-  // and create the bottom and left axes inside of them
-  chartGroup.append("g")
-    .call(leftAxis);
-
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
-
-  // Create one SVG rectangle per piece of tvData
-  // Use the linear and band scales to position each rectangle within the chart
-  chartGroup.selectAll(".circle")
-    .data(stateData)
-    .enter()
-    .append("circle")
-    .attr("class", "bar")
-    .attr("cx", d => xLinearScale(d.name))
-    .attr("cy", d => yLinearScale(d.hours))
-    .attr("r", "12")
-    .attr("fill", "green")
-    .attr("opacity", ".6");  
-
-
-  */
-
-}).catch(function(error) {
+}).catch(function (error) {
     console.log(error);
-  });
+});
 
 
 
@@ -81,4 +90,4 @@ d3.csv("./assets/resources/data.csv").then(function(stateData) {
     "globals": {
       "d3": true
     }
-  }*/  
+  }*/
