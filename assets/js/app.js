c8ba9a93eff26d3c5cb9b1da073ec7d7a4a2ffc4
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-03-11 12:41:38 -0500

    add linear scales and scatter
    found misplaced ',' in  csv

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,54 @@ d3.csv("./assets/resources/data.csv").then(function(stateData) {
     // CHeck to see what imported data looks like
     console.log(stateData);
     
-});
+  /*
+  // Cast the hours value to a number for each piece of tvData
+  stateData.forEach(function(d) {
+    d.hours1 = +d.hours1;
+    d.hours2 = +d.hours2;
+  });
+  
+  // Create a linear scales for axis
+  var xLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.hours)])
+    .range([chartHeight, 0]);
+  
+  var yLinearScale = d3.scaleLinear()
+  .domain([0, d3.max(stateData, d => d.hours)])
+  .range([chartHeight, 0]);
+
+  // Create axis function
+  var bottomAxis = d3.axisBottom(xBandScale);
+  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);  
+
+  // Append two SVG group elements to the chartGroup area,
+  // and create the bottom and left axes inside of them
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
+
+  // Create one SVG rectangle per piece of tvData
+  // Use the linear and band scales to position each rectangle within the chart
+  chartGroup.selectAll(".circle")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("class", "bar")
+    .attr("cx", d => xLinearScale(d.name))
+    .attr("cy", d => yLinearScale(d.hours))
+    .attr("r", "12")
+    .attr("fill", "green")
+    .attr("opacity", ".6");  
+
+
+  */
+
+}).catch(function(error) {
+    console.log(error);
+  });
 
 
 
